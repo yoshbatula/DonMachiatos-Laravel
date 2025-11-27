bd0e0521991c90f8d3f463df1015f0801e692323
yoshbatula <yoshbatula2@gmail.com>
2025-11-27 14:25:09 +0800

    progress for the don machiatos kiosk

---
+++ DonMachiatos/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Don Machos</title>
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+            <style>
+                
+            </style>
+        @endif
+    </head>
+    <body>
+        <div className="flex items-center justify-center">
+            <div class="bg-black"></div>
+        </div>  
+    </body>i
+</html>
