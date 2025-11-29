473799af378375467596e4d7cda0ac8730703231
yoshbatula <yoshbatula2@gmail.com>
2025-11-29 18:49:39 +0800

    done for the side menu selection

--- DonMachiatos/resources/views/components/Menu.blade.php
+++ DonMachiatos/resources/views/components/Menu.blade.php
@@ -1,11 +1,40 @@
 <div class="flex flex-row gap-4">
 {{-- Menu Selection --}}
-    <div class="mt-5 px-[50px] grid grid-cols-1 bg-white rounded-[40px] shadow-md h-180 w-50 transform translate-x-[50px]">
+    <div class="mt-5 grid grid-cols-1 bg-white rounded-[40px] shadow-md h-180 w-50 transform translate-x-[50px] overflow-hidden">
         <div class="flex flex-col text-center">
-            <div class="mt-8">
+            <div class="mt-8 px-[50px]">
                 <h1 class="font-bold text-[24px]">Menu</h1>
             </div>
-            <div class="text-start bg-gray-50 border border-gray-50"></div>
+            <div class="w-full h-0.5 bg-gray-300 transform translate-y-[15px]"></div>
+            <div class="flex flex-col items-center transform translate-y-[55px]">
+                <button class="hover:cursor-pointer">
+                    <img class="w-20" src={{ Vite::asset('resources/images/NewLogo.svg') }} alt="What's new">
+                    <h1 class="text-sm font-medium mt-2">What's new</h1>
+                </button>
+            </div>
+            <div class="flex flex-col items-center transform translate-y-[80px]">
+                <button class="hover:cursor-pointer">
+                    <img class="w-20" src={{ Vite::asset('resources/images/Coffee.svg') }} alt="What's new">
+                    <h1 class="text-sm font-medium mt-2">Coffee</h1>
+                </button>
+            </div>
+            <div class="flex flex-col items-center transform translate-y-[100px]">
+                <button class="hover:cursor-pointer">
+                    <img class="w-20" src={{ Vite::asset('resources/images/Coffee.svg') }} alt="What's new">
+                    <h1 class="text-sm font-medium mt-2">Frappe</h1>
+                </button>
+            </div>
+            <div class="flex flex-col items-center transform translate-y-[120px]">
+                <button class="hover:cursor-pointer">
+                    <img class="w-20" src={{ Vite::asset('resources/images/Coffee.svg') }} alt="What's new">
+                    <h1 class="text-sm font-medium mt-2">Coolers</h1>
+                </button>
+            </div>
+            <div class="flex flex-col items-center transform translate-y-[170px]">
+                <button class="hover:cursor-pointer">
+                    <img class="w-10" src={{ Vite::asset('resources/images/ArrowDown.svg') }} alt="What's new">
+                </button>
+            </div>
         </div>
     </div>
     {{-- Menu Selection --}}
