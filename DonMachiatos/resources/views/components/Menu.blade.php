b54b134964aa537b647a60ff567aa08ba2b36ffd
yoshbatula <yoshbatula2@gmail.com>
2025-11-29 18:32:16 +0800

    progress for the din-in features

--- DonMachiatos/resources/views/components/Menu.blade.php
+++ DonMachiatos/resources/views/components/Menu.blade.php
@@ -1,11 +1,17 @@
-<div class="flex flex-row">
+<div class="flex flex-row gap-4">
 {{-- Menu Selection --}}
     <div class="mt-5 px-[50px] grid grid-cols-1 bg-white rounded-[40px] shadow-md h-180 w-50 transform translate-x-[50px]">
         <div class="flex flex-col text-center">
             <div class="mt-8">
                 <h1 class="font-bold text-[24px]">Menu</h1>
             </div>
-            <div class="bg-gray-50 border border-gray-50"></div>
+            <div class="text-start bg-gray-50 border border-gray-50"></div>
+        </div>
+    </div>
+    {{-- Menu Selection --}}
+    <div class="flex flex-row bg-white rounded-[40px] h-180 w-125 shadow-md translate-x-[70px] translate-y-[20px]">
+        <div>
+
         </div>
     </div>
 </div>
