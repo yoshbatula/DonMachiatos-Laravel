7821e7565996e675b2f5b9b5565d5ba6376d2c15
yoshbatula <yoshbatula2@gmail.com>
2025-12-14 22:23:02 +0800

    just fixing some bugs

--- DonMachiatos/app/Http/Controllers/OrderControllers/OrderCont.php
+++ DonMachiatos/app/Http/Controllers/OrderControllers/OrderCont.php
@@ -13,20 +13,16 @@ use App\Models\Payment;
             
             try {
                 
-                // Check if cart is empty
                 if (Carts::count() === 0) {
                     return redirect()->route('dinein')->with('error', 'Your cart is empty. Please add items before proceeding to checkout.');
                 }
                 
-                // Get all cart items
                 $cartItems = Carts::all();
                 
-                // Calculate total amount
                 $totalAmount = $total = $cartItems->sum(function($item) {
                     return $item->productPrice * $item->productQuantity;
                 });
                 
-                // Create new order
                 $orders = new Order();
                 $orders->CartID = $cartItems->first()->CartID;
                 $orders->TotalAmount = $totalAmount;
